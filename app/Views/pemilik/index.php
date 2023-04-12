18a76fd6176f6e26481b286499d8dd2c44f522be
nimrod-cmd <marcelimanuel70@gmail.com>
2023-04-12 11:13:06 +0700

    third commit

--- app/Views/pemilik/index.php
+++ app/Views/pemilik/index.php
@@ -6,7 +6,7 @@
 <div class="container-fluid">
 
 <!-- Page Heading -->
-<h1 class="h3 mb-4 text-gray-800">User list</h1>
+<h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
 
 </div>
 <?= $this->endSection(); ?>
